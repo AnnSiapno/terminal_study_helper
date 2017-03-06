5984954bc30ef9928d0763b1aa38f15e20176f9e
Ann Siapno <ann.n.siapno@gmail.com>
2017-03-06 19:51:35 +1100

    Incorporated TTY Prompt for the categories selection
    Incorporated TTY Prompt for selecting the category

--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
 require 'terminal-table'
 require 'artii'
-require_relative './classes.rb'
-require_relative './arrays.rb'
 require_relative './methods.rb'
 
 title = Artii::Base.new :font => 'slant'
